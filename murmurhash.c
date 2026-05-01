1188b5cac69bd72d2ad289ae7cc5ce5c963fb02f
Wu-Haonan <hnw.bio@outlook.com>
2026-05-01 16:55:22 -0400

    small typo in murmurhash.c

--- murmurhash.c
+++ murmurhash.c
@@ -7,7 +7,7 @@
 // compile and run any of them on any platform, but your performance with the
 // non-native version will be less than optimal.
 
-#include "murmurHash.h"
+#include "murmurhash.h"
 #include <stdint.h>
 #include <stdlib.h>
 
